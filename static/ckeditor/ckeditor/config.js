8929841a426e149868cb832c2deff57f8d5bb7ee
moses group <mosesgroup00@gmail.com>
2019-03-26 12:01:44 +0300

    update ckeditor

--- static/ckeditor/ckeditor/config.js
+++ static/ckeditor/ckeditor/config.js
@@ -3,8 +3,11 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.enterMode = CKEDITOR.ENTER_BR; // pressing the ENTER KEY input <br/>
+    config.shiftEnterMode = CKEDITOR.ENTER_P; //pressing the SHIFT + ENTER KEYS input <p>
+    config.autoParagraph = false; // stops automatic insertion of <p> on focus
 };
